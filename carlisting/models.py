2bde330505a3c146d423aa7b53180b816dc32364
Aditya Rai <adityarai23bs@gmail.com>
2024-04-06 17:38:01 +0545

    added database and admin panel

--- carlisting/models.py
+++ carlisting/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+CAR_TYPE = (
+    ('SUV', 'SUV'),
+    ('EV', 'EV'),
+    ('Truck', 'Truck'),
+    ('Sedan', 'Sedan'),
+)
+
 # Create your models here.
+class CarDetails(models.Model):
+    renter_name = models.CharField(max_length=100, null=True)
+    renter_contact = models.CharField(max_length=100, null=True)
+    car_type = models.CharField(max_length=100, choices=CAR_TYPE, null=True)
+    car_model = models.CharField(max_length=100, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.renter_name} - {self.car_type} - {self.car_model} - {self.availability}'
+
