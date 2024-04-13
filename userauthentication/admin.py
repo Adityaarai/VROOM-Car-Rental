ebad182174620a1789aa2f6f2efb51eb15360d1e
Aditya Rai <adityarai23bs@gmail.com>
2024-04-13 13:44:28 +0545

    updated the database and carlisting page

--- userauthentication/admin.py
+++ userauthentication/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Distributor, Vroom_User
 
 # Register your models here.
+
+class DistributorsAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'contact', 'email')
+
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'contact', 'email')
+
+admin.site.register(Distributor, DistributorsAdmin)
+admin.site.register(Vroom_User, UsersAdmin)
