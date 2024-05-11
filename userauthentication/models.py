737d01d5ec05e1ad0fbb8b0c3f996ee1f075c452
Aditya Rai <adityarai23bs@gmail.com>
2024-05-12 00:30:27 +0545

    added signals to create user profiles

--- userauthentication/models.py
+++ userauthentication/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    address = models.CharField(max_length=100, null=True)
+    license_number = models.CharField(max_length=100, null=True)
+    contact = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} - Profile'
